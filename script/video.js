773c0737078e702d1f5c7d5f0d4dcd25403fe01f
aysha3f <tahiaysha220@gmail.com>
2025-08-21 21:42:25 +0600

    Load Categories And Send Data To Display

---
+++ script/video.js
@@ -0,0 +1,20 @@
+// 1- Fetch, Load and show categories on html
+
+// create loadCatagories
+const loadCatagories = () =>{
+
+    // fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res) => res.json())
+    .then((data)=> DisPlayCategories(data.categories))
+    .catch((error) => console.log(error));
+
+}
+// Create DisPlayCategories
+const DisPlayCategories = (data) => {
+    // add Data in html
+    console.log(data);
+
+};
+
+loadCatagories();
