f80436b71048eb86622e1defa497fc15627ac71c
aysha3f <tahiaysha220@gmail.com>
2025-08-26 02:48:42 +0600

    Format Time And Display Conditional Time

--- script/video.js
+++ script/video.js
@@ -1,5 +1,13 @@
-// 1- Fetch, Load and show categories on html
+function getTimeString(time){
+   // get hours and rest seconds
+   const hour = parseInt(time/3600);
+   let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+   remainingSecond = remainingSecond % 60;
+  return `${hour} hour ${minute} minute ${remainingSecond} second ago  `
+}
 
+// 1- Fetch, Load and show categories on html
 // create loadCatagories
 const loadCatagories = () =>{
 
@@ -50,7 +58,10 @@ const videoContainer = document.getElementById("videos");
       src=${video.thumbnail}
       class =h-full w-full object-cover"
       alt="Shoes" />
-      <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
+      ${
+        video.others.posted_date?.length == 0 ? "" : ` <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString( video.others.posted_date)}</span>`
+      }
+     
   </figure>
   <div class="px-0 py-2 flex gap-2">
   <div>
